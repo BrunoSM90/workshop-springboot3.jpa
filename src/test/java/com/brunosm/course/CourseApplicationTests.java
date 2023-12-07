fa95a0edb398b5da5d1ef4831882b4f9c4985159
Bruno Salgado Mazzola <smbrunosm@gmail.com>
2023-12-07 20:06:10 -0300

    project creation

---
+++ src/test/java/com/brunosm/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunosm.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
