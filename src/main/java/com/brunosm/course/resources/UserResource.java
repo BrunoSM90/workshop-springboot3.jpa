f4a6709f02c8c31f7d04713e341ef0ccf6c0b858
Bruno Salgado Mazzola <smbrunosm@gmail.com>
2023-12-08 19:48:38 -0300

    User resource creation (get)

---
+++ src/main/java/com/brunosm/course/resources/UserResource.java
@@ -0,0 +1,23 @@
+package com.brunosm.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.brunosm.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		System.out.println("Acionou1");
+		User u = new User(2, "Bruno", "smbrunosm@gmail.com", "991614045", "1234");
+		System.out.println(u);
+		
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
