41b8fb4006db03dccb234465d3165f8a1b8779e5
Bruno Salgado Mazzola <smbrunosm@gmail.com>
2023-12-08 21:11:29 -0300

    H2 data seeding

--- src/main/java/com/brunosm/course/resources/UserResource.java
+++ src/main/java/com/brunosm/course/resources/UserResource.java
@@ -13,7 +13,7 @@ public class UserResource {
 	
 	@GetMapping
 	public ResponseEntity<User> findAll() {
-		User u = new User(2, "Bruno", "smbrunosm@gmail.com", "991614045", "1234");
+		User u = new User(1L, "Bruno", "smbrunosm@gmail.com", "991614045", "1234");
 		
 		return ResponseEntity.ok().body(u);
 	}
