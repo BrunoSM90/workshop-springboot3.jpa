31b4cdb4531c27a3401cc0d76f417a764ebccbf8
Bruno Salgado Mazzola <smbrunosm@gmail.com>
2023-12-12 10:33:39 -0300

    exceção para update

--- src/main/java/com/brunosm/course/services/UserService.java
+++ src/main/java/com/brunosm/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.brunosm.course.repositories.UserRepository;
 import com.brunosm.course.services.exceptions.DatabaseException;
 import com.brunosm.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	@Autowired
@@ -44,9 +46,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = userRepository.getReferenceById(id);
-		updateData(entity, obj);
-		return userRepository.save(entity);
+		try {
+			User entity = userRepository.getReferenceById(id);
+			updateData(entity, obj);
+			return userRepository.save(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 	
 	public void updateData(User entity, User obj) {
